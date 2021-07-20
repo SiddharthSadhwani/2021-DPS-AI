fd2ebd5e44ce9e42a7d35ffe453091fcb0273b5d
sidddddd-18 <siddharth.sadhwani@gmail.com>
2021-07-20 22:37:34 +0530

    Task Completed

--- code.js
+++
@@ -1,5 +0,0 @@
-import { totp } from 'D:/IIITD/dps_task/node_modules/hotp-totp-generator/index.js';
-
-const totpToken = totp({ key: 'siddharth18313@iiitd.ac.inDPSCHALLENGE', X: 120 , T0: 0, algorithm: 'sha512'});
-console.log(totpToken);
-console.log(hotpOtpGenerator);
